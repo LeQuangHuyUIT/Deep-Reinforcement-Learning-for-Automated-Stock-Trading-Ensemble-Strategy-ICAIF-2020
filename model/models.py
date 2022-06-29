f1839a85bd35d14969ed7afc4e99a7f8602d04cc
LeQuangHuy.UIT <19521617@gm.uit.edu.vn>
2022-06-29 20:03:36 +0700

    change PPO policy

--- model/models.py
+++ model/models.py
@@ -69,7 +69,7 @@ def train_PPO(env_train, model_name, timesteps=50000):
     """PPO model"""
 
     start = time.time()
-    model = PPO2('MlpLstmPolicy', env_train, ent_coef = 0.005, nminibatches = 8)
+    model = PPO2('MlpPolicy', env_train, ent_coef = 0.005, nminibatches = 8)
     #model = PPO2('MlpPolicy', env_train, ent_coef = 0.005)
 
     model.learn(total_timesteps=timesteps)
