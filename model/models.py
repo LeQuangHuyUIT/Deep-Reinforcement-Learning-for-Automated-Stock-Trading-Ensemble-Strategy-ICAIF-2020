9dff9a7f971aac33942d0aa5591f845b389031d8
LeQuangHuy.UIT <19521617@gm.uit.edu.vn>
2022-07-03 21:52:27 +0700

    tuning a2c

--- model/models.py
+++ model/models.py
@@ -29,7 +29,7 @@ def train_A2C(env_train, model_name, timesteps=25000):
     """A2C model"""
 
     start = time.time()
-    model = A2C('MlpPolicy', env_train, verbose=0)
+    model = A2C('MlpPolicy', env_train, verbose=0, n_steps=128, momentum=0.5)
     model.learn(total_timesteps=timesteps)
     end = time.time()
 
