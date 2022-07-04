54a35e96c152bcb41a28a0b78b350b22e0d50f3a
LeQuangHuy.UIT <19521617@gm.uit.edu.vn>
2022-07-04 12:57:29 +0700

    debug

--- model/models.py
+++ model/models.py
@@ -31,7 +31,7 @@ def train_A2C(env_train, model_name, timesteps=25000):
     """A2C model"""
 
     start = time.time()
-    model = A2C('MlpPolicy', env_train, verbose=0, n_steps=10, momentum=0.5, learning_rate= 0.0000025)
+    model = A2C('MlpPolicy', env_train, verbose=1, n_steps=10, momentum=0.5, learning_rate= 0.0000025)
     model.learn(total_timesteps=timesteps)
     end = time.time()
 
@@ -100,13 +100,13 @@ def train_GAIL(env_train, model_name, timesteps=1000):
     print('Training time (GAIL): ', (end - start) / 60, ' minutes')
     return model
 
-def train_SAC(env_train, model_name, time_steps=40000):
-    model = SAC(MlpPolicy, env_train, verbose=1)
+def train_SAC(env_train, model_name, timesteps=40000):
     n_actions = env_train.action_space.shape[-1]
     # param_noise = None
     action_noise = OrnsteinUhlenbeckActionNoise(mean=np.zeros(n_actions), sigma=float(0.5) * np.ones(n_actions))
     start = time.time()
-    model.learn(total_timesteps=time_steps, log_interval=10, action_noise= action_noise)
+    model = SAC(MlpPolicy, env_train, verbose=1, action_noise= action_noise)
+    model.learn(total_timesteps=timesteps, log_interval=10)
     end = time.time()
     model.save(f"{config.TRAINED_MODEL_DIR}/{model_name}")
     print('Training time (SAC): ', (end - start) / 60, ' minutes')
@@ -237,14 +237,14 @@ def run_ensemble_strategy(df, unique_trade_date, rebalance_window, validation_wi
         # print("==============Model Training===========")
 
 
-        print("======SAC Training========")
-        model_a2c = train_SAC(env_train, model_name="SAC_100k_dow_{}".format(i), timesteps=100000)
-        print("======SAC Validation from: ", unique_trade_date[i - rebalance_window - validation_window], "to ",
+        print("======A2C Training========")
+        model_a2c = train_A2C(env_train, model_name="A2C_100k_dow_{}".format(i), timesteps=10000)
+        print("======A2C Validation from: ", unique_trade_date[i - rebalance_window - validation_window], "to ",
               unique_trade_date[i - rebalance_window])
         env_val = DummyVecEnv([lambda: StockEnvValidation(validation,
                                                           turbulence_threshold=turbulence_threshold,
                                                           iteration=i,
-                                                          model_name="SAC")])
+                                                          model_name="A2C")])
         obs_val = env_val.reset()
         DRL_validation(model=model_a2c, test_data=validation, test_env=env_val, test_obs=obs_val)
         sharpe_a2c = get_validation_sharpe(i, "SAC")
